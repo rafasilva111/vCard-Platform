1693aaabb8bcefe3a2183b5ae8eaba27cdd7b036
rafael <rafael.francisco111@gmail.com>
2021-11-16 16:25:15 +0000

    initial commit 2

---
+++ Controllers/ContaController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Results;
+using vCardPlatform.Models;
+
+namespace vCardPlatform.Controllers
+{
+    public class ContaController : ApiController
+    {
+        string connectionString = "Data Source=(LocalDB)/MyInstance;AttachDbFilename=C:/Users/rafae/source/repos/vCardPlatform/App_Data/Database.mdf;Integrated Security=True";
+        //Co
+
+
+
+        // GET: Conta/GetContaById/1
+        public IHttpActionResult GetContaById(int id)
+        {
+            SqlConnection connection = null;
+
+            try
+            {
+                connection = new SqlConnection(connectionString);
+                connection.Open();
+                string cmdSQL = "SELECT * FROM Table WHERE Id=@idPedidosTable";
+                SqlCommand command = new SqlCommand(cmdSQL, connection);
+                command.Parameters.AddWithValue("@idPedidosTable", id);
+                SqlDataReader reader = command.ExecuteReader();
+
+                Conta pedidoAInserir = null;
+
+                while (reader.Read())
+                {
+                    pedidoAInserir = new Conta();
+                    pedidoAInserir.Id = (int)reader["Id"];
+                    pedidoAInserir.Balance = (float)reader["Balance"];
+                    pedidoAInserir.AccountOwner = (string)reader["AccountOwner"];
+                    pedidoAInserir.CreatedAt = (DateTime)reader["Balance"];
+
+                }
+
+                reader.Close();
+                connection.Close();
+                if (pedidoAInserir != null)
+                {
+                    return Ok(pedidoAInserir);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception e)
+            {
+                if (connection.State == System.Data.ConnectionState.Open)
+                {
+                    connection.Close();
+                }
+
+                return Ok(e.Message);
+            }
+        }
+
+       
+        
+    }
+}
---
+++ Models/Conta.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vCardPlatform.Models
+{
+    public class Conta
+    {
+        public int Id { get; set; }
+        public string AccountOwner { get; set; }
+        public float Balance { get; set; }
+        public DateTime CreatedAt{get;set;}
+
+
+    }
+}
